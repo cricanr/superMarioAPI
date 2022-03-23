c9efe8d594ef79799086b527cb1edc190ce1acff
radu.crican <radu.crican@konux.de>
2022-03-23 13:05:06 +0100

    Cleanup code a bit, added service class

--- app/controllers/SuperMarioCharactersAsyncController.scala
+++ app/controllers/SuperMarioCharactersAsyncController.scala
@@ -1,11 +1,11 @@
 package controllers
 
-import csvparser.CSVFilePath.{PowernessCSVFilePath, SpeedCSVFilePath}
 import csvparser.SuperMarioCharactersParser
-import models.{SearchRequest, SuperMarioCharacter, SuperMarioCharacterPowerModel, SuperMarioCharacterSpeedModel}
+import models.{SearchRequest, SuperMarioCharacter}
 import play.api.libs.json._
 import play.api.mvc._
 import query.QueryParameters
+import services.SuperMarioCharactersService
 
 import javax.inject._
 import scala.concurrent.ExecutionContext
@@ -13,38 +13,18 @@ import scala.concurrent.ExecutionContext
 @Singleton
 class SuperMarioCharactersAsyncController @Inject()(cc: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(cc) {
   private val superMarioCharactersParser = new SuperMarioCharactersParser()
+  private val superMarioCharactersService = new SuperMarioCharactersService()
+
+  import superMarioCharactersService._
 
   def getAllNames: Action[AnyContent] = Action {
-    val allPowerItems = superMarioCharactersParser.readAllItems(PowernessCSVFilePath)
-    val powerItems = allPowerItems.map(powerItemRaw =>
-      SuperMarioCharactersParser.getSuperMarioCharactersPowerFromCsvLine(powerItemRaw)
-    )
-    val allCharacters = powerItems.map(powerItem => powerItem.character)
-
+    val allCharacters = superMarioCharactersService.getAllNames
     Ok(Json.toJson(allCharacters).toString())
   }
 
   def getAllCharactersSorted: Action[AnyContent] = Action { request =>
     val maybeSortOrderParam = QueryParameters(request.queryString)
-
-    val powerItems = readPowerItems()
-    val speedItems = readSpeedItems()
-
-    val allCharacters = powerItems.keys.map(key =>
-      SuperMarioCharacter(
-        name = key,
-        firstGame = powerItems(key).firstGame,
-        power = calculatePower(speedItems(key),
-          powerItems(key)),
-        speed = speedItems(key).speed)
-    ).toList
-
-    val sortedCharacters = maybeSortOrderParam match {
-      case Some("asc") => allCharacters.sortBy(characters => characters.power)
-      case Some("desc") => allCharacters.sortBy(characters => characters.power)(Ordering[Double].reverse)
-      case None => allCharacters
-    }
-
+    val sortedCharacters = superMarioCharactersService.getAllCharactersSorted(maybeSortOrderParam)
     Ok(Json.toJson(sortedCharacters).toString())
   }
 
@@ -57,22 +37,13 @@ class SuperMarioCharactersAsyncController @Inject()(cc: ControllerComponents)(im
 
     maybeSearchRequest.map {
       case JsSuccess(searchRequest, _) =>
-        val foundCharacterKeys = powerItems.keys.toList.filter(a => searchRequest.names.contains(a))
-        val foundCharacters = foundCharacterKeys.map(characterName => SuperMarioCharacter(name = characterName, firstGame = powerItems(characterName).firstGame, power = calculatePower(speedItems(characterName), powerItems(characterName)), speed = speedItems(characterName).speed)).toList
+        val foundCharacters = filterCharacters(searchRequest, powerItems, speedItems)
         Ok(Json.toJson(foundCharacters).toString())
       case JsError(errors) => InternalServerError(errors.toString())
     }.getOrElse {
-      val foundCharacters = powerItems.keys.map(characterName =>
-        SuperMarioCharacter(
-          name = characterName,
-          firstGame = powerItems(characterName).firstGame,
-          power = calculatePower(speedItems(characterName),
-            powerItems(characterName)),
-          speed = speedItems(characterName).speed
-        )).toList
+      val foundCharacters = composeCharacterItems(powerItems, speedItems)
       Ok(Json.toJson(foundCharacters).toString())
     }
-
   }
 
   def create: Action[AnyContent] = Action { request =>
@@ -103,25 +74,5 @@ class SuperMarioCharactersAsyncController @Inject()(cc: ControllerComponents)(im
     }.getOrElse {
       BadRequest("Invalid input given in body")
     }
-  }
-
-  private def calculatePower(speedItem: SuperMarioCharacterSpeedModel, powerItem: SuperMarioCharacterPowerModel): Double = {
-    powerItem.powerfulness * 100 / speedItem.speed
-  }
-
-  private def readSpeedItems(): Map[String, SuperMarioCharacterSpeedModel] = {
-    val allSpeedItems = superMarioCharactersParser.readAllItems(SpeedCSVFilePath)
-    val speedItems = allSpeedItems.map(speedItemRaw =>
-      SuperMarioCharactersParser.getSuperMarioCharactersSpeedFromCsvLineMap(speedItemRaw)
-    ).toMap
-    speedItems
-  }
-
-  private def readPowerItems(): Map[String, SuperMarioCharacterPowerModel] = {
-    val allPowerItems = superMarioCharactersParser.readAllItems(PowernessCSVFilePath)
-    val powerItems = allPowerItems.map(powerItemRaw =>
-      SuperMarioCharactersParser.getSuperMarioCharactersPowerFromCsvLineMap(powerItemRaw)
-    ).toMap
-    powerItems
   }
 }
